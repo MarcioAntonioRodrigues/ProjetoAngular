22094114e26cbd8380ddc95d63b7f7d3e87e3da4
marcio <ciorma7@hotmail.com>
2019-05-21 01:15:50 -0300

    criacao do login component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MatButtonModule} from '@angular/material/button';
 import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { AppRoutingModule } from './app-routing.module';
+import { LoginComponent } from './login/login.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import 'hammerjs';
@@ -15,15 +16,16 @@ import 'hammerjs';
   declarations: [
 	AppComponent,
 	HomeComponent,
+	LoginComponent,
 	HeaderComponent,
 	FooterComponent
   ],
   imports: [
     BrowserModule,
-	AppRoutingModule,
-	BrowserAnimationsModule,
-	MatButtonModule,
-	MatInputModule
+		AppRoutingModule,
+		BrowserAnimationsModule,
+		MatButtonModule,
+		MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
