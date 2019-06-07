fa202096351f06bfa1319d7e4006f121243f2827
Marcio Antônio Rodrigues de Souza <marcio.souza@minds.local>
2019-06-07 18:45:49 -0300

    finalizando tela de compra

--- src/app/purchase/purchase.component.ts
+++ src/app/purchase/purchase.component.ts
@@ -12,17 +12,27 @@ import { SessionService } from '../services/session.service';
 export class PurchaseComponent implements OnInit
 {
     view = 0;
+	meia = 0;
+	kmVtg = 0;
+	total = 0;
+	inteira = 0;
+	selectMeia = 0;
     purchase: any;
     pecas: any = [];
     filmes: any = [];
-	eventsService: EventsService;
-	inteira = 0;
-	kmVtg = 0;
-	meia = 0;
+	selectedCard:any;
 	selectInteira = 0;
-	selectMeia = 0;
 	selectVantagem = 0;
-	total = 0;
+	creditCard = 
+	{
+		number: null,
+		name: null,
+		cvv: null,
+		month: null,
+		year: ""
+	}
+	eventsService: EventsService;
+	cardYear = ['Jan', 'Fev', 'Mar'];
     
     constructor(eventsService:EventsService, 
                 private route: ActivatedRoute,
@@ -30,11 +40,14 @@ export class PurchaseComponent implements OnInit
     {
         this.filmes = eventsService.jsonList;
 		this.pecas = eventsService.pecas;
+		this.creditCard.year = "Ano";
     }
 
     ngOnInit()
     {
-        this.purchase = this.sessionService.getPurchase();
+		this.purchase = this.sessionService.getPurchase();
+		// this.creditCard.year = "Ano";
+		console.log(this.creditCard.year)
     }
 
     changeView(val)
@@ -44,6 +57,7 @@ export class PurchaseComponent implements OnInit
 	
 	calcTicket()
 	{
+		debugger
 		let vtg = 12;
 		let halph = 14;
 		this.total = 0;
@@ -56,5 +70,7 @@ export class PurchaseComponent implements OnInit
 		this.kmVtg = vtg;
 		this.total = this.inteira + this.meia + this.kmVtg;
 		console.log(this.total);
+		console.log(this.creditCard)
 	}
+
 }
